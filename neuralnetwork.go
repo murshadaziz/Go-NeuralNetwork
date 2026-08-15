538ff2ef4a0d9e2d2fd1404013a4cb28e2c88c67
murshadaziz <mubutt.bese24seecs@seecs.edu.pk>
2026-08-15 19:18:18 +0500

    Tested dataloading

--- neuralnetwork.go
+++ neuralnetwork.go
@@ -5,20 +5,45 @@ import (
 )
 
 func main() {
-	var layer1 Layer
-	layer1.init(4, 3, relu)
-	var layer2 Layer
-	layer2.init(3, 2, softmax)
+	/*
+		var layer1 Layer
+		layer1.init(4, 3, relu)
+		var layer2 Layer
+		layer2.init(3, 2, softmax)
 
-	var neuralnetwork NeuralNetwork
-	neuralnetwork = append(neuralnetwork, layer1)
-	neuralnetwork = append(neuralnetwork, layer2)
+		var neuralnetwork NeuralNetwork
+		neuralnetwork = append(neuralnetwork, layer1)
+		neuralnetwork = append(neuralnetwork, layer2)
 
-	neuralnetwork.Randomise()
+		neuralnetwork.Randomise()
 
-	input := []float64{1, 2, 3, 4}
-	output := neuralnetwork.ForwardProgation(input)
+		input := []float64{1, 2, 3, 4}
+		output := neuralnetwork.ForwardProgation(input)
 
-	fmt.Println(output)
+		fmt.Println(output)
+	*/
+
+	images, err := readIdxImagesFloat64("dataset/train-images.idx3-ubyte")
+	if err != nil {
+		fmt.Println("error reading images:", err)
+		return
+	}
+
+	labels, err := readIdxLabelsFloat64("dataset/train-labels.idx1-ubyte")
+	if err != nil {
+		fmt.Println("error reading labels:", err)
+		return
+	}
+
+	fmt.Println("num images:", len(images))
+	fmt.Println("num labels:", len(labels))
+	fmt.Println("pixels per image:", len(images[0]))
+
+	fmt.Println("first label:", labels[0])
+	fmt.Println("first image (first 28 pixels):")
+	for i := 0; i < 28; i++ {
+		fmt.Printf("%.1f ", images[0][i])
+	}
+	fmt.Println()
 
 }
